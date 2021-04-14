c386c5bc5c159288a62fe7d68fc4b1f46174c644
sungwwoo <p11120076@gmail.com>
2021-04-14 11:04:44 +0900

    wk7

---
+++ wk7/3/under_damped.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+float func(float t){		// analytic solution
+	return exp(-t)*(cos(3*sqrt(11)*t) - (sqrt(11)*sin(3*sqrt(11)*t))/33);
+}
+int main(){
+	float dt = 0.01;
+	float x2, x0 = 1;		// x2 = x[n+2], x1 = x[n+1], x0 = x[n]
+	float x1 = dt * 0 + x0; // x1 = dt * x'(0) + x0
+	float k = 10;
+	const float m = 0.1, b = 0.2;
+
+	ofstream output("under_damped.txt");
+
+	output << 0 << " " << x0 << " " << func(0.0) << endl;
+	output << dt << " " << x1 << " " << func(dt) << endl;
+	for (float t = 2 * dt; t < 5.0; t += dt){
+		x2 = -(-2 + b * dt / m + pow(dt,2) * k / m) * x1 -(1 - b * dt / m) * x0;
+		output << t << " " << x2 << " " << func(t) << endl;
+		x0 = x1;
+		x1 = x2;
+	}
+
+	return 0;
+}
