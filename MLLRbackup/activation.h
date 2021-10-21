7539ffc70bf11bb0d2cb847dfee1b4fa552a7687
sungwwoo <p11120076@gmail.com>
2021-10-22 06:11:16 +0900

    END

--- MLLRbackup/activation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Eigen/Dense>
-using namespace Eigen;
-class activation {
-public:
-	static int step(double x);
-	static double sigmoid(double x);
-	static MatrixXd softmax(MatrixXd x, int m, int n);
-};
